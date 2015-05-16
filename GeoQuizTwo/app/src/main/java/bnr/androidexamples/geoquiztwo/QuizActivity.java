e90b9eff16c54edcbb84c18b18ddeec00497c870
Patrick DeChant <pdechant7@gmail.com>
2015-05-16 13:15:33 -0400

    Ch 1 - Added member variable references to widgets, set listeners on them and added Toasts.

--- GeoQuizTwo/app/src/main/java/bnr/androidexamples/geoquiztwo/QuizActivity.java
+++ GeoQuizTwo/app/src/main/java/bnr/androidexamples/geoquiztwo/QuizActivity.java
@@ -4,14 +4,42 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        // set listener for TrueButton
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        // set listener for FalseButton
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
